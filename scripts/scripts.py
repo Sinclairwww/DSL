4c5f2508a43008935f20ebec04c33f4ccaffd5ad
Sinclairwww <90683011+Sinclairwww@users.noreply.github.com>
2023-12-26 15:42:54 +0800

    add comments

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -7,45 +7,54 @@ import sqlite3
 # 调用getInstance类方法获取全局的runpy实例
 runpy = RunPy.getInstance()
 
+
 # 编写完脚本函数以后，只需要加上runpy.register装饰器并传入你想赋予的
 # 脚本名称就可以了，之后便可以使用callpy命令调用
-@runpy.register('GetName')
+@runpy.register("GetName")
 def getname(number):
-    conn = sqlite3.connect('data/random_users.db')
+    conn = sqlite3.connect("data/random_users.db")
     cur = conn.cursor()
-    cur = cur.execute('select name from users where number = (?)', (number,))
+    cur = cur.execute("select name from users where number = (?)", (number,))
     res = cur.fetchone()[0]
     conn.close()
     return res
 
-@runpy.register('GetBalance')
+
+# 获取用户余额
+@runpy.register("GetBalance")
 def getbalance(number):
-    conn = sqlite3.connect('data/random_users.db')
+    conn = sqlite3.connect("data/random_users.db")
     cur = conn.cursor()
-    cur = cur.execute('select balance from users where number = (?)', (number,))
+    cur = cur.execute("select balance from users where number = (?)", (number,))
     res = cur.fetchone()[0]
     conn.close()
     return res
 
-@runpy.register('UploadComplaint')
+
+# 上传投诉
+@runpy.register("UploadComplaint")
 def uploadcomplaint(complaint):
-    conn = sqlite3.connect('data/complaints.db')
+    conn = sqlite3.connect("data/complaints.db")
     cur = conn.cursor()
-    cur = cur.execute('insert into complaints values (?)', (complaint,))
+    cur = cur.execute("insert into complaints values (?)", (complaint,))
     conn.commit()
     conn.close()
 
-@runpy.register('Topup')
+
+# 为用户充值
+@runpy.register("Topup")
 def topup(number):
-    conn = sqlite3.connect('data/random_users.db')
+    conn = sqlite3.connect("data/random_users.db")
     cur = conn.cursor()
-    cur = cur.execute('update users set balance = balance + (?)', (int(number),))
+    cur = cur.execute("update users set balance = balance + (?)", (int(number),))
     conn.commit()
     conn.close()
 
-@runpy.register('weather')
+
+# 从wttr.in获取天气信息
+@runpy.register("weather")
 def weather(loc):
-    resp = requests.get('http://wttr.in/{}?format=3'.format(loc))
+    resp = requests.get("http://wttr.in/{}?format=3".format(loc))
     if resp.status_code == 404:
         return ""
     else:
