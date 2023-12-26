192f0918c3f5d9baac80ccd95d03cce4f48884d7
Sinclairwww <90683011+Sinclairwww@users.noreply.github.com>
2023-12-26 20:37:20 +0800

    modified ASTNode

--- src/ply/AST.py
+++ src/ply/AST.py
@@ -5,13 +5,3 @@ class ASTNode:
     def __init__(self, type, *childs):
         self.type = type
         self.childs = list(childs)
-
-    # 递归打印节点与子节点
-    def print(self, depth=0):
-        print("\t" * depth, end="")
-        print(self.type)
-        for child in self.childs:
-            # 如果子节点不是ASTNode，抛出异常
-            if not isinstance(child, ASTNode):
-                raise RuntimeError(child)
-            child.print(depth=depth + 1)
